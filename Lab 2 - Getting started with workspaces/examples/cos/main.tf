0bd98862357d0d6a198d10a7034d9427f5e27d97
geethasathyamurthy <43596586+geethasathyamurthy@users.noreply.github.com>
2021-07-15 15:24:04 +0530

    Update main.tf

--- Lab 2 - Getting started with workspaces/examples/cos/main.tf
+++ Lab 2 - Getting started with workspaces/examples/cos/main.tf
@@ -19,23 +19,9 @@ resource "ibm_cos_bucket_object" "plaintext" {
   key             = "plaintext.txt"
 }
 
-/** doctopus APIKey
-provider "ibm" {
-  region           = "us-south"
-  ibmcloud_api_key = "oUbbxF5qpss8eSYrzYssiOl2k8VykD64B8eR8gRQXNaD"
-}*/
 
 provider "ibm" {
   region           = "us-south"
   ibmcloud_api_key = "36z5sG4bjbJX03SmjnbCAfrtEauRnhfixONuLnSvcKl6"
 }
 
-/**  You can try this part of the block to store an object into COS bucket
-resource "ibm_cos_bucket_object" "file" {
-  bucket_crn      = ibm_cos_bucket.cos_bucket.crn
-  bucket_location = ibm_cos_bucket.cos_bucket.region_location
-  content_file    = "${path.module}/helper/object.json"
-  key             = "file.json"
-  etag            = filemd5("${path.module}/helper/object.json")
-}
-*/
